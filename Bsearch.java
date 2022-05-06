2b861dd354c358ecf347c2eb15ae40fa64d0f846
ReetuChauhann <chauhanreetu00@gmail.com>
2022-05-06 15:07:59 +0530

    binary search using recursion

---
+++ Bsearch.java
@@ -0,0 +1,27 @@
+public class Bsearch {
+    public static void main(String[] args) {
+        int[] arr = {12,14,15,77,89};
+        int target=14;
+        int start=0;
+        int end=arr.length-1;
+        int ans=binaerSearch(arr,target,start,end);
+        System.out.println(ans);
+    }
+    static int binaerSearch(int[] arr, int target, int start, int end){
+
+        if(start>end){
+            return -1;
+        }
+        int m = start+(end-start)/2;
+        if(target==arr[m]){
+            return m;
+        }
+        if(target<arr[m]){
+            return binaerSearch(arr, target, start, m-1);
+        }else{
+            return binaerSearch(arr, target, m+1, end);
+        }
+        
+    }
+    
+}
